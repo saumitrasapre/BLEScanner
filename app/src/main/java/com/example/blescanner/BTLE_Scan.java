4607ccc618f3d7430bfc7fc504cea8fc2f4e9192
Saumitra <saumitrasapre69@gmail.com>
2020-01-09 12:34:04 +0530

    Proximities updates in arrays

--- app/src/main/java/com/example/blescanner/BTLE_Scan.java
+++ app/src/main/java/com/example/blescanner/BTLE_Scan.java
@@ -30,6 +30,7 @@ import org.altbeacon.beacon.distance.ModelSpecificDistanceCalculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import static java.lang.Math.pow;
 
@@ -43,13 +44,26 @@ public class BTLE_Scan {
     private Handler myhandler;
     private int signalStrength;
     private long scanPeriod;
-    ArrayList<Integer>distArray=new ArrayList<Integer>(3);
+   int distArray[]=new int[10];
+   static int rssi1;
+   static int rssi2;
+   static int rssi3;
 
     public BTLE_Scan(MainActivity main, long scanPeriod, int signalStrength) {
         this.main = main;
         this.signalStrength = signalStrength;
         this.scanPeriod = scanPeriod;
         myhandler = new Handler();
+
+        distArray[0]=0;
+        distArray[1]=0;
+        distArray[2]=0;
+        distArray[3]=0;
+
+        rssi1=-10;
+        rssi2=-10;
+        rssi3=-10;
+
 
         final BluetoothManager bluetoothManager = (BluetoothManager) main.getSystemService(Context.BLUETOOTH_SERVICE);
 
@@ -110,12 +124,15 @@ public class BTLE_Scan {
         public void onScanResult(int callbackType, final ScanResult result) {
             super.onScanResult(callbackType, result);
             final int new_rssi = result.getRssi();
+
+
             myhandler.post(new Runnable() {
                 @Override
                 public void run() {
 
                     processScanResult(result);
                     System.out.println(result.getDevice().getAddress());
+
                     main.addDevice(result.getDevice(), new_rssi);
 
 
@@ -139,32 +156,35 @@ public class BTLE_Scan {
     private void processScanResult(@NonNull ScanResult scanResult) {
         String macAddress = scanResult.getDevice().getAddress();
         byte[] data = scanResult.getScanRecord().getBytes();
+
         ScanRecord record=scanResult.getScanRecord();
         int txPowerlevel=record.getTxPowerLevel();
-        int rssi=scanResult.getRssi();
+
+
         AndroidModel model=AndroidModel.forThisDevice();
+
         //ModelSpecificDistanceCalculator myobj=new ModelSpecificDistanceCalculator(main.getApplicationContext(),null,model);
         double n = 2.25;
         //double distance;
 
         //distance=  Math.pow(10.0, ((double)txPowerlevel - rssi) / (10 * 2));
-        double distance=0;
-        if (rssi == 0 || txPowerlevel == 0) {
-            distance = -1;
-        }
-        double ratio2 = txPowerlevel - rssi;
-        double ratio2_linear = Math.pow(10, ratio2 / 10);
-        double y = 0;
-         double r = Math.sqrt(ratio2_linear);
-
-        double ratio = rssi * 1.0 / txPowerlevel;
-       // if (ratio < 1.0)
-      //  {
-      //      y = Math.pow(ratio, 10);
-      //  }
-       // else
-        //{
-            y = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+//        double distance=0;
+//        if (rssi == 0 || txPowerlevel == 0) {
+//            distance = -1;
+//        }
+//        double ratio2 = txPowerlevel - rssi;
+//        double ratio2_linear = Math.pow(10, ratio2 / 10);
+//        double y = 0;
+//         double r = Math.sqrt(ratio2_linear);
+//
+//        double ratio = rssi * 1.0 / txPowerlevel;
+//       // if (ratio < 1.0)
+//      //  {
+//      //      y = Math.pow(ratio, 10);
+//      //  }
+//       // else
+//        //{
+//            y = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
             //distance= y;
       // }
 
@@ -172,56 +192,64 @@ public class BTLE_Scan {
         //if(scanResult.getDevice().getName()=="Adi") {
 
         // }
-        int flag = 0;
-        if(scanResult.getDevice().getName()=="Beacon1" ||scanResult.getDevice().getName()=="Beacon2" ||scanResult.getDevice().getName()=="Beacon3" ) {
+        int flag1=0;
+        int flag2=0;
+        int flag3 =0;
+        //String devName=scanResult.getDevice().getName();
+      // if(scanResult.getDevice().getName().equals("Beacon1") ||scanResult.getDevice().getName().equals("Beacon2") ||scanResult.getDevice().getName().equals("Beacon3") ) {
 
-            if (scanResult.getDevice().getName() == "Beacon1") {
-                if (rssi <= 60) {
-                    flag = 0;
+            System.out.println("Henlo");
+            if(scanResult.getDevice().getName()!=null) {
+                if (scanResult.getDevice().getName().equals("Beacon1") ) {
+                    if (rssi1 <= -60) {
+                        flag1 = 0;//far
+                    }
+
+                    if (rssi1 > -60) {
+                        flag1 = 1;
+                    }
+
+                    if (scanResult.getDevice().getName().equals("Beacon1")) {
+                        distArray[1] = flag1;
+                    }
+
                 }
 
-                if (rssi > 60) {
-                    flag = 1;
+                if (scanResult.getDevice().getName().equals("Beacon2")) {
+                    if (rssi2 <= -60) {
+                        flag2 = 0;
+                    }
+
+                    if (rssi2 > -60) {
+                        flag2 = 1;
+                    }
+
+                    if (scanResult.getDevice().getName().equals("Beacon2")) {
+                        distArray[2] = flag2;
+                    }
+
                 }
 
-                if (scanResult.getDevice().getName() == "Beacon1") {
-                    distArray.add(0, flag);
-                }
+                if (scanResult.getDevice().getName().equals("Beacon3")) {
+                    if (rssi3 <= -65) {
+                        flag3 = 0;
+                    }
 
+                    if (rssi3 > -65) {
+                        flag3 = 1;
+                    }
+
+                    if (scanResult.getDevice().getName().equals("Beacon3")) {
+                        distArray[3] = flag3;
+                    }
+
+                }
+                Log.d("Dis Array : ", "[ " + String.valueOf(distArray[1]) + String.valueOf(distArray[2]) + String.valueOf(distArray[3]) + "]");
             }
 
-            if (scanResult.getDevice().getName() == "Beacon2") {
-                if (rssi <= 60) {
-                    flag = 0;
-                }
+            //  Log.d("Dis Array : ","[ "+ rssi1 +rssi2 + rssi3 + "]");
 
-                if (rssi > 60) {
-                    flag = 1;
-                }
-
-                if (scanResult.getDevice().getName() == "Beacon2") {
-                    distArray.add(1, flag);
-                }
-
-            }
-
-            if (scanResult.getDevice().getName() == "Beacon3") {
-                if (rssi <= 65) {
-                    flag = 0;
-                }
-
-                if (rssi > 65) {
-                    flag = 1;
-                }
-
-                if (scanResult.getDevice().getName() == "Beacon3") {
-                    distArray.add(2, flag);
-                }
-
-            }
-            Log.d("Dis Array : ","[ "+ String.valueOf(distArray.get(0)) +String.valueOf( distArray.get(1)) + String.valueOf(distArray.get(2)) + "]");
-
-        }
+        //}
 
 
         //   Log.d("Distance ", "ADDRESS  " + scanResult.getDevice().getName() + " distance is  " + String.valueOf(distance) + "TX Power "+String.valueOf(txPowerlevel));
